fab587ed064f56a528e420c28145710d6e57b373
nwhunguk <nwhunguk@gmail.com>
2022-04-14 15:00:21 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function testGreaterThan(val) {
+    if (val >= 20) {  
+        console.log("20 or over");
+    }  
+    if (val >= 10) {
+        console.log("10 or over");
+  }
+  else  
+  
+  console.log("less than 10");
+   }
+
+  
+  testGreaterThan(20);
